acd1eff798fe43b6dfb313c5a1e53813a14ae235
ggase54 <romanboskatov405@gamil.com>
2022-07-19 14:10:53 +0700

    practice-2

--- service/models.py
+++ service/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
 
@@ -11,11 +12,16 @@ class Post(models.Model):
   def __str__(self):
     return f'{self.title}'
 
+  def get_absolute_url(self):
+    return reverse('index')
+
 class Comment(models.Model):
   description = models.TextField(verbose_name="Описание поста", null=True, blank=True)
-  post = models.ForeignKey(Post, related_name="Comments", on_delete=models.CASCADE)
+  post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
   created_at = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
-
 
   def __str__(self):
     return f'{self.description}'
+
+  def get_absolute_url(self):
+    return reverse('index')
