ca99bbc9592eeb993d462f5489bb6b286a55e34c
ggase54 <romanboskatov405@gamil.com>
2022-07-31 14:02:10 +0700

    check permission for user

--- service/views.py
+++ service/views.py
@@ -3,10 +3,10 @@ from .models import Post, Comment
 from django.views.generic import ListView, DeleteView, CreateView, UpdateView, DetailView
 from .forms import PostForm, CommentForm, UserRegisterForm
 from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.contrib.auth.decorators import login_required, permission_required, user_passes_test
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 
-@login_required
+
 def about(req):
   return render(req, 'about.html')
 
@@ -15,7 +15,7 @@ class PostsView(ListView):
   template_name = 'index.html'
   ordering = ['created_at']
 
-class RegisterForm(CreateView, LoginRequiredMixin):
+class RegisterForm(CreateView):
   form_class = UserRegisterForm
   template_name = 'register.html'
   success_url = reverse_lazy('login')
@@ -24,22 +24,25 @@ class DetailPostView(DetailView):
   model = Post
   template_name = 'detail_post.html'
 
-class CreatePostView(CreateView, LoginRequiredMixin):
+class CreatePostView(PermissionRequiredMixin, CreateView):
+  permission_required = 'service.add_post'
   model = Post
   template_name = 'create_post.html'
   form_class = PostForm
 
-class UpdatePostView(UpdateView, LoginRequiredMixin):
+class UpdatePostView(PermissionRequiredMixin, UpdateView):
+  permission_required = 'service.update_post'
   model = Post 
   template_name = 'update_post.html'
   form_class  = PostForm
 
-class DeletePostView(DeleteView, LoginRequiredMixin):
+class DeletePostView(PermissionRequiredMixin, DeleteView):
+  permission_required = 'service.delete_post'
   model = Post 
   template_name = 'delete_post.html'
   success_url = reverse_lazy('index')
 
-class AddCommentView(CreateView, LoginRequiredMixin):
+class AddCommentView(LoginRequiredMixin, CreateView):
   model = Comment 
   template_name = 'add_comment.html'
   form_class = CommentForm
